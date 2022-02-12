552339bbc60db2b80887f1c44c8e29e3cafa43e6
anton.sergeev.4568 <anton.sergeev.4568@gmail.com>
2022-02-12 19:09:42 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+
+template <class T>
+T Max(T a, T b)
+{
+	if (a > b) return a;
+	return b;
+}
+
+template <class P>
+P Min(P a, P b)
+{
+	if (a < b) return a;
+	return b;
+}
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	int i1, i2, maxInt, minInt;
+
+	cout << "Введите int:" << ::endl;
+	cout << "i1 = ";
+	cin >> i1;
+	cout << "i2 = ";
+	cin >> i2;
+
+	maxInt = Max(i1, i2);
+	minInt = Min(i1, i2);
+
+	cout << "Максимальное значение = " << maxInt << endl;
+	cout << "Минимальное значение = " << minInt << endl;
+
+	// 
+	float f1, f2, maxFloat, minFloat;
+
+	cout << "Введите floats:" << ::endl;
+	cout << "f1 = ";
+	cin >> f1;
+	cout << "f2 = ";
+	cin >> f2;
+
+	maxFloat = Max(f1, f2);
+	minFloat = Min(f1, f2);
+
+	cout << "Максимальное значение = " << maxFloat << endl;
+	cout << "Минимальное значение = " << minFloat << endl;
+	system("pause");
+}
